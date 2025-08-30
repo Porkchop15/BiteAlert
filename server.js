05824b5c2518c11983176ad7750c540262e60303
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2025-08-30 18:09:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ const allowedOrigins = [
   'http://192.168.1.10:3000',
   'http://localhost',
   'http://192.168.1.10',
-  'http://localhost:57660',      // Flutter Web on Chrome
-  'http://127.0.0.1:57660',      // Flutter Web on Chrome (alternate)
+  'http://localhost:60818',      // Flutter Web on Chrome
+  'http://127.0.0.1:60818',      // Flutter Web on Chrome (alternate)
   'capacitor://localhost',
   'ionic://localhost',
   'http://localhost:8080',

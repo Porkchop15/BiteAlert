f69f0c5aedc40a362199b5e93d9755faa78e2f96
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2025-10-15 18:54:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(cors({
     return callback(null, true);
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Staff-Name', 'X-Staff-Center'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Staff-Name', 'X-Staff-Center', 'X-Staff-Id', 'X-Audit-Intent'],
   credentials: true,
   maxAge: 86400 // 24 hours
 }));
